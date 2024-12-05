9fcc5ec9cb9f3d48091bb5eca868ae933c24a912
alisher <dev1ant_b@vk.com>
2024-12-05 20:21:00 +0500

    FIXED

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -5,6 +5,18 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('', lambda request: redirect('contact/')),  # Redirect root URL to 'contact/'
     path('contact/', views.contact_view, name='contact'),  # URL path is 'contact/'
+    path('blog/', views.Blog.as_view(), name='blog'),
+    path('cart/', views.Cart.as_view(), name='cart'),
+    path('category/', views.Category.as_view(), name='category'),
+    path('checkout/', views.Checkout.as_view(), name='checkout'),
+    path('confirmation/', views.Confirmation.as_view(), name='confirmation'),
+    path('elements/', views.Elements.as_view(), name='elements'),
+    path('index/', views.Index.as_view(), name='index'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('single-blog/', views.Singleblog.as_view(), name='single-blog'),
+    path('single-product/', views.Singleproduct.as_view(), name='single-product'),
+    path('tracking/', views.Tracking.as_view(), name='tracking'),
+
 
 
 ] + staticfiles_urlpatterns()
--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,6 +1,8 @@
+from operator import index
+
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView
 from .form import ContactForm
 from .models import ContactPage
 from django import forms
@@ -15,3 +17,26 @@ def contact_view(request):
         form = ContactForm()  # Render an empty form for GET request
 
     return render(request, 'karma/contact.html', {'form': form})
+
+class Blog(TemplateView):
+    template_name = 'karma/Blog.html'
+class Cart(TemplateView):
+    template_name = 'karma/cart.html'
+class Category(TemplateView):
+    template_name = 'karma/category.html'
+class Checkout(TemplateView):
+    template_name = 'karma/checkout.html'
+class Confirmation(TemplateView):
+    template_name = 'karma/confirmation.html'
+class Elements(TemplateView):
+    template_name = 'karma/elements.html'
+class Index(TemplateView):
+    template_name = 'karma/index.html'
+class Login(TemplateView):
+    template_name = 'karma/login.html'
+class Singleblog(TemplateView):
+    template_name = 'karma/single-blog.html'
+class Singleproduct(TemplateView):
+    template_name = 'karma/single-product.html'
+class Tracking(TemplateView):
+    template_name = 'karma/tracking.html'
